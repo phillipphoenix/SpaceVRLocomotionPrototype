c5fc170a082a162ec4e06634f493feb41f5ce6af
Martin <me_thomsen@hotmail.com>
2016-09-15 00:43:49 +0200

    First part of locomotion (rope) done

--- SpaceVRLocomotionPrototype/Assets/Scripts/Controller.cs
+++ SpaceVRLocomotionPrototype/Assets/Scripts/Controller.cs
@@ -12,6 +12,7 @@ public class Controller : MonoBehaviour {
     private Transform playerJoint;
 
     private bool _isHolding;
+    private bool triggerClicked;
     private GrabPoint currentlyGrabbing;
 
     public void Awake() {
@@ -24,6 +25,7 @@ public class Controller : MonoBehaviour {
         controller.TriggerUnclicked += TriggerUnclicked;
         controller.TriggerUsed += OnTriggerUsed;
         controller.Gripped += GripPressed;
+        controller.MenuButtonClicked += MenuButtonPressed;
         cameraRig = GameObject.FindGameObjectWithTag("CameraRig").GetComponent<CameraRig>();
         player = Camera.main.transform;
         playerJoint = GameObject.FindGameObjectWithTag("PlayerJoint").transform;
@@ -39,6 +41,7 @@ public class Controller : MonoBehaviour {
         controller.TriggerUnclicked -= TriggerUnclicked;
         controller.TriggerUsed -= OnTriggerUsed;
         controller.Gripped -= GripPressed;
+        controller.MenuButtonClicked -= MenuButtonPressed;
     }
 
     public void OnTriggerEnter(Collider collider) {
@@ -65,28 +68,38 @@ public class Controller : MonoBehaviour {
         }
     }
 
-    private void GripPressed(object sender, ClickedEventArgs e)
+    private void MenuButtonPressed(object sender, ClickedEventArgs e)
     {
-        cameraRig.GetComponent<Rigidbody>().AddForce(-cameraRig.transform.forward * 10);
+        cameraRig.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        playerJoint.transform.position = new Vector3(0, 1, 0);
     }
 
-    private void TriggerClicked(object sender, ClickedEventArgs e) {
+    private void GripPressed(object sender, ClickedEventArgs e)
+    {
+    }
+
+    private void TriggerClicked(object sender, ClickedEventArgs e)
+    {
+        triggerClicked = true;
         if (hoveredHandle != null) {
          
 
         }
     }
 
-    private void TriggerUnclicked(object sender, ClickedEventArgs e) {
-
+    private void TriggerUnclicked(object sender, ClickedEventArgs e)
+    {
+        triggerClicked = false;
     }
 
     private void OnTriggerUsed(object sender, TriggerValueArgs e)
     {
-        if (e.value > 0.5f)
+        if (e.value > 0.3f && !triggerClicked)
         {
-            if (_isHolding) 
-                Grab(currentlyGrabbing);
+            if (_isHolding)
+            {
+                UpdateGrab(currentlyGrabbing);
+            }
             else if (hoveredHandle != null)
                 Grab(hoveredHandle);
         }
@@ -102,6 +115,11 @@ public class Controller : MonoBehaviour {
         target.Grab(Vector3.Distance(transform.position, playerJoint.position));
         currentlyGrabbing = target;
         _isHolding = true;
+    }
+
+    void UpdateGrab(GrabPoint target)
+    {
+        target.UpdateGrab(Vector3.Distance(transform.position, playerJoint.position));
     }
 
     void Release()
